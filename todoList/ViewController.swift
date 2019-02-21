f06477b58022e9ef0839b6cf3de55bad17c65f0c
Anna Guo <annaguo03@gmail.com>
2019-02-21 18:12:32 -0500

    5.2 Commit: - Added bar button items, navigation controllers, segues, and cancel button

--- todoList/ViewController.swift
+++ todoList/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,4 +19,3 @@ class ViewController: UIViewController {
 
 
 }
-
